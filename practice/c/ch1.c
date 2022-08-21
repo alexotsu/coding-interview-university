1aaeed079a725b536de6fd6fc175921030fb1b4c
Alex Otsu <alexotsu@gmail.com>
2022-08-21 14:45:22 -0700

    uploading materials

---
+++ practice/c/ch1.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+
+// // Exercise 1-1. Run the "hello, world" program on your system. Experiment
+// // with leaving out parts of the program, to see what error messages you get.
+
+// main()
+// {
+//   printf("hello, world\n");
+// }
+
+
+// // Exercise 1-2. Experiment to find out what happens when printf's argument
+// // string contains \c, where c is some character not listed above.
+
+// main() 
+
+// {
+//   printf("hello, world\n");
+// }
+
+// // format specifiers: https://www.tutorialspoint.com/format-specifiers-in-c
+
+// main() {
+//   float fahr, celsius;
+//   int lower, upper, step;
+
+//   lower = 0;
+//   upper = 300;
+//   step = 20;
+
+//   fahr = lower;
+//   while (fahr <= upper) {
+//     celsius = (5.0/9.0) * (fahr-32.0);
+//     printf("%3.0f %6.1f\n", fahr, celsius);
+//     fahr = fahr + step;
+//   }
+// }
+
+// // Exercise 1-7. Write a program to print the value of EOF
+// // reference code:
+// main() {
+// 	int c, nl;
+// 	while ((c = getchar()) != EOF)
+//     if (c == '\n')
+//       ++nl;
+// 		// putchar(c);
+//   printf("%d\n", nl);
+// }
+
+// main() {
+//   printf("%3d", EOF);
+// }
+
+
+
+// - Exercise 1.8. Write a program to count blanks, tabs, and newlines.
+main() {
+  int c, blank, tab, newline;
+  blank = 0;
+  tab = 0;
+  newline = 0;
+  // printf("%10s %10s %10s\n", "Blanks", "Tabs", "Newlines");
+  // printf("%d %d %d\n", blank, tab, newline);
+  while((c = getchar()) != EOF)
+    // printf("%10d %10d %10d\n", blank, tab, newline);
+    // printf("%d", c);
+    if(c == ' ') {
+      ++blank;
+    }
+    if(c == '\t') {
+      ++tab;
+    }
+    if(c == '\n') {
+      ++newline;
+    }
+    fflush(stdout);
+    printf("\n%10s %10s %10s\n", "Blanks", "Tabs", "Newlines"); 
+    fflush(stdout);
+    printf("\n%10d %10d %10d\n", blank, tab, newline);
+    fflush(stdout);
+
+}
+
+// - Exercise 1-9. Write a program to copy its input to its output, replacing each
+// string of one or more blanks by a single blank.
+
+// - Exercise 1-10. Write a program to copy its input to its output, replacing each
+// tab by \ t, each backspace by \b, and each backslash by \ \. This makes tabs
+// and backspaces visible in an unambiguous way.
+
