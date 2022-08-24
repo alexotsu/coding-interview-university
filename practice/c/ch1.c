f942249c01a69e5078faf32ac8da4381f9bae951
Alex Otsu <alexotsu@gmail.com>
2022-08-24 00:04:24 -0700

    Progress on practice problems

--- practice/c/ch1.c
+++ practice/c/ch1.c
@@ -53,82 +53,245 @@
 
 
 
-// - Exercise 1.8. Write a program to count blanks, tabs, and newlines.
-int main(void) {
-  int c, blank, tab, newline;
-  blank = 0;
-  tab = 0;
-  newline = 0;
-  printf("%10s %10s %10s\n", "Blanks", "Tabs", "Newlines"); 
-  while((c = getchar()) != EOF) {
-    if(c == ' ')
-      ++blank;
-    if(c == '\t')
-      ++tab;
-    if(c == '\n')
-      ++newline;
-    printf("%10d %10d %10d\n", blank, tab, newline);
-  }
+// // - Exercise 1.8. Write a program to count blanks, tabs, and newlines.
+// int main(void) {
+//   int c, blank, tab, newline;
+//   blank = 0;
+//   tab = 0;
+//   newline = 0;
+//   printf("%10s %10s %10s\n", "Blanks", "Tabs", "Newlines"); 
+//   while((c = getchar()) != EOF) {
+//     if(c == ' ')
+//       ++blank;
+//     if(c == '\t')
+//       ++tab;
+//     if(c == '\n')
+//       ++newline;
+//     printf("%10d %10d %10d\n", blank, tab, newline);
+//   }
 
-  return 0;
-}
+//   return 0;
+// }
 
-// - Exercise 1-9. Write a program to copy its input to its output, replacing each
-// string of one or more blanks by a single blank.
+// // - Exercise 1-9. Write a program to copy its input to its output, replacing each
+// // string of one or more blanks by a single blank.
 
-// - Exercise 1-10. Write a program to copy its input to its output, replacing each
-// tab by \ t, each backspace by \b, and each backslash by \ \. This makes tabs
-// and backspaces visible in an unambiguous way.
+// # define BLANK      1
+// # define NOTBLANK   0
+// int main(void) {
+//   int c;
+//   int lastChar = NOTBLANK;
+//   while((c = getchar()) != EOF) {
+//     if(c != ' ')
+//       lastChar = NOTBLANK;
+//     if(lastChar == NOTBLANK)
+//       putchar(c);
+//     if(c == ' ')
+//       lastChar = BLANK;
+//   }
 
-// // 1.13:
-// #define SIZE 5    /* size of lengths array */
-// #define SCALE 1   /* adjust to accommodate large input */
-// #define OUT  1    /* outside of a word */
-// #define IN   0    /* inside of a word */
+//   return 0;
+// }
+
+// // - Exercise 1-10. Write a program to copy its input to its output, replacing each
+// // tab by \t, each backspace by \b, and each backslash by \\. This makes tabs
+// // and backspaces visible in an unambiguous way.
+// // @note wasn't really supposed to use else if but it's ok
+// # define BACKSPACE '\b'
+// # define TAB '\t'
+// # define BACKSLASH '\\'
+
+// int main(void) {
+//   int c;
+//   while((c = getchar()) != EOF) {
+//     if(c == BACKSPACE)
+//       printf("%s", "\\b");
+//     else if(c == TAB)
+//       printf("%s", "\\t");
+//     else if(c == BACKSLASH)
+//       printf("%s", "\\\\");
+//     else
+//       putchar(c);
+//   }
+// }
+
+// #define YES 1
+// #define NO  0 
 
 // int main(void)
 // {
-// 	int c, i, j, count, state;
-// 	int lengths[SIZE]; /* words length ranges */
+// 	int c, charReplaced; 
 
-// 	for (i = 0; i <= SIZE; ++i)
-// 		lengths[i] = 0;
-
-// 	state = OUT;
-// 	count = 0;
 // 	while ((c = getchar()) != EOF) {
-
-// 		if (c == ' ' || c == '\t' || c == '\n')
-// 			state = OUT;
-// 		else
-// 			state = IN;
-
-// 		if (state == IN)
-// 			++count;
-
-// 		if (state == OUT) {
-// 			if (count < 4)
-// 				++lengths[0];
-// 			else if (count >= 4 && count < 8)
-// 				++lengths[1];
-// 			else if (count >= 8 && count < 12)
-// 				++lengths[2];
-// 			else if (count >= 12 && count < 14)
-// 				++lengths[3];
-// 			if (count >= 14)
-// 				++lengths[4];
-// 			count = 0;
+// 		charReplaced = NO;
+// 		if (c == '\t') {
+// 			printf("\\t");
+// 			charReplaced = YES;
 // 		}
-// 	}	
-
-// 	printf("\nHorizontal Histogram\n");
-// 	for (i = 0; i < SIZE; ++i) {
-// 		printf(" %i\t", lengths[i]);
-// 		for (j = 0; j < lengths[i] / SCALE; ++j)
-// 			printf(" *");
-// 		printf("\n");
+// 		if (c == '\b') {
+// 			printf("\\b");
+// 			charReplaced = YES;
+// 		}
+// 		if (c == '\\') {
+// 			printf("\\\\");
+// 			charReplaced = YES;
+// 		}
+// 		if (charReplaced == NO)
+// 			putchar(c);
 // 	}
-
 // 	return 0;
 // }
 
+
+// // Exercise 1-12. Write a program that prints its input one word per line.
+
+// # define IN 1
+// # define OUT 0
+
+// int main(void) {
+//   int c, nl, nw, nc, state;
+
+//   state = OUT;
+//   nl = nw = nc = 0;
+//   while((c = getchar()) != EOF) {
+//     ++nc;
+
+//     if(c == '\n') {
+//       ++nl;
+//     }
+
+//     if(c == ' ' || c == '\n' || c == '\t') {
+//       state = OUT;
+//     } else if (state == OUT) {
+//       printf("\n");
+//       state = IN;
+//       ++nw;
+//     }
+
+//     if(state == IN ) {
+//       putchar(c);
+//     }
+    
+//   }
+// }
+
+// // Exercise 1-13. Write a program to print a histogram of the lengths of words in
+// // its input. It is easy to draw the histogram with the bars horizontal; a vertical
+// // orientation is more challenging.
+
+// // 1. find word boundaries
+// // 2. find word length
+
+// # define IN 1
+// # define OUT 0
+// # define MAX_LENGTH 8
+
+// int main(void) {
+//   int c, i, wordLength, state;
+//   int arrayLength = MAX_LENGTH + 1;
+//   int wordLengths[MAX_LENGTH + 1];
+
+//   state = IN;
+//   wordLength = i = 0;
+//   for(i; i < arrayLength; ++i) {
+//     wordLengths[i] = 0;
+//   }
+
+//   while((c = getchar()) != EOF) {
+
+//     if(c == ' ' || c == '\t' || c == '\n') {
+//       state = OUT;
+//     }
+
+//     if(state == IN) {
+//       ++wordLength;
+//     }
+    
+//     if (state == OUT) {
+//       if(wordLength > MAX_LENGTH) {
+//         wordLength = MAX_LENGTH;
+//       }
+//       if(wordLength > 0) {
+//         ++wordLengths[wordLength];
+//       }
+//       wordLength = 0;
+//       state = IN;
+//     }
+
+//     if(c == '\n'){
+//       for(i = 1; i < arrayLength; ++i) {
+//         if(i == MAX_LENGTH) {
+//           printf("%d+: ", i);  
+//         } else {
+//           printf("%d:  ", i);
+//         }
+//         if (wordLengths[i] > 0) {
+//           printf("%0*d\n", wordLengths[i], 0);
+//         } else {
+//           printf("\n");
+//         }
+//       }
+//     }
+
+//   }
+// }
+
+// // Exercise 1-14. Write a program to print a histogram of the frequencies of different characters in its input.
+
+// # define CHARS 256
+
+// int main(void) {
+//   int charCount[CHARS];
+//   int i = 0;
+//   int c;
+//   for(i = 0; i < CHARS; ++i) {
+//     charCount[i] = 0;
+//   }
+
+//   while((c = getchar()) != EOF) {
+//     ++charCount[c];
+//     if(c == '\n'){
+//       for(i = 0; i < CHARS; ++i) {
+//         if (charCount[i] > 0) {
+//           if (i == '\n') {
+//             printf("newline:");
+//           } else if (i == ' ') {
+//             printf("%7s:", "space");
+//           } else if (i == '\t') {
+//             printf("%7s:", "tab");
+//           } else {
+//             printf("%7c:", i);
+//           }
+//           printf("%0*d\n", charCount[i], 0);
+//         }
+//       }
+//     }
+//   }
+
+// }
+
+// Exercise 1-15. Rewrite the temperature conversion program of Section 1.2 to
+// use a function for conversion
+
+# define LOWER 0
+# define UPPER 300
+# define STEP 20
+
+float fahrToCel(int fahr);
+
+int main(void) {
+  float fahr, cel;
+  fahr = LOWER;
+
+  while(fahr <= UPPER) {
+    cel = fahrToCel(fahr);
+    printf("%3.0f %6.1f\n", fahr, cel);
+    fahr = fahr + STEP;
+  }
+
+}
+
+float fahrToCel(int fahr) {
+  float cel = (5.0/9.0) * (fahr-32.0);
+  return cel;
+}
